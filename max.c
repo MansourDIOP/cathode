069a1a794e388c010e58b5072268916e27848571
MansourDIOP <mansourd549@gmail.com>
2020-11-10 15:57:20 +0000

    fonction max

---
+++ max.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int a=0,b=0;
+    printf("entrer la premiere valeur\n");
+    scanf("%d\n",&a);
+    printf("enter la deuxieme valeur\n");
+    scanf("%d\n",&b);
+}
